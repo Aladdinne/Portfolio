03c6f57135b7c852cd8253049620cb47a93138df
Aladdinne <aladin.hammouda@esprit.tn>
2024-09-15 01:58:04 +0100

    add

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -31,9 +31,18 @@ function ResumeNew() {
           </Button>
         </Row>
 
-        <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+        <Row className="resume ">
+          <Document file={pdf} className={"d-flex flex-column align-items-center justify-content-center"}>
+            <Page
+                pageNumber={1}
+                scale={width > 786 ? 1.7 : 0.6}
+                style={{ marginBottom: '30px' }} // Adds space between pages
+            />
+            <hr/>
+            <Page
+                pageNumber={2}
+                scale={width > 786 ? 1.7 : 0.6}
+            />
           </Document>
         </Row>
 
